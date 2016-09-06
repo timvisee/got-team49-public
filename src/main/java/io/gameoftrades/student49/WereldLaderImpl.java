2f3cf74baed368c4fdde0c44eb69995fdaf49ae8
Tim Visee <timvisee@gmail.com>
2016-09-06 11:00:36 +0200

    Load map object

--- src/main/java/io/gameoftrades/student49/WereldLaderImpl.java
+++ src/main/java/io/gameoftrades/student49/WereldLaderImpl.java
@@ -1,10 +1,12 @@
 package io.gameoftrades.student49;
 
 import io.gameoftrades.model.Wereld;
+import io.gameoftrades.model.kaart.Kaart;
 import io.gameoftrades.model.lader.WereldLader;
 
 import java.io.InputStream;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WereldLaderImpl implements WereldLader {
@@ -12,35 +14,34 @@ public class WereldLaderImpl implements WereldLader {
     @Override
     public Wereld laad(String resource) {
         // Get the input stream for the given resource
-        InputStream in = this.getClass().getResourceAsStream(resource);
+        final InputStream in = this.getClass().getResourceAsStream(resource);
 
         // Make sure a valid resource is loaded
         if(in == null)
             throw new RuntimeException("The given resource file does not exist, the program will be terminated");
 
         // Create a scanner for the loaded resource
-        Scanner scan = new Scanner(in);
+        final Scanner scan = new Scanner(in);
 
         // Read the map size
-        String rawMapSize = scan.next();
+        final String rawMapSize = scan.next();
 
         // Make sure the raw map size has a valid format
-        Pattern mapSizePattern = Pattern.compile("");
+        final Pattern mapSizePattern = Pattern.compile("^([0-9]+),([0-9]+)$");
+        final Matcher mapSizeMatcher = mapSizePattern.matcher(rawMapSize);
 
-        // Split the raw map size
-        String[] splitMapSize = rawMapSize.split(",");
-
-        // Make sure we've two split parts
-        if(splitMapSize.length != 2)
+        // Find the pattern and make sure it exists
+        if(!mapSizeMatcher.find())
             throw new RuntimeException("The world being loaded has a malformed map size");
 
-        // TODO: Make sure the raw map dimensions contain numbers, possibly use a regex
-
         // Get the map width and height
-        final int mapWidth = Integer.parseInt(splitMapSize[0]);
-        final int mapHeight = Integer.parseInt(splitMapSize[1]);
+        final int mapWidth = Integer.parseInt(mapSizeMatcher.group(0));
+        final int mapHeight = Integer.parseInt(mapSizeMatcher.group(1));
 
-        // TODO: Load map
+        // Create a map
+        final Kaart map = new Kaart(mapWidth, mapHeight);
+
+        // TODO: Load the map tiles (terrain), and add them to the map object
 
         // TODO: Load towns
 
