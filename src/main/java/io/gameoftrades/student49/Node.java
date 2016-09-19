f87fefdfb320c342234e74839333195b6a025f3c
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-09-19 14:49:53 +0200

    Add parent and child nodes

--- src/main/java/io/gameoftrades/student49/Node.java
+++ src/main/java/io/gameoftrades/student49/Node.java
@@ -6,9 +6,18 @@ import io.gameoftrades.model.kaart.Terrein;
 public class Node {
 
 	private Terrein terrein;
+	private Node 	parent;
+	private Node 	child;
 	private int 	gCost;
 	private double  hCost;
 	private double  fCost;
+	
+	public Node(Terrein terrein) {
+		this.terrein = terrein;
+		this.gCost = 0;
+		this.hCost = 0.0;
+		this.fCost = 0.0;
+	}
 	
 	public Node(Terrein terrein, Coordinaat eind) {
 		this.terrein = terrein;
@@ -31,5 +40,21 @@ public class Node {
 	
 	public double getFCost() {
 		return this.fCost;
+	}
+	
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+	
+	public Node getParent() {
+		return this.parent;
+	}
+	
+	public void setChild(Node child) {
+		this.child = child;
+	}
+	
+	public Node getChild() {
+		return this.child;
 	}
 }
