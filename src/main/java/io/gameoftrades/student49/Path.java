d9b21e2ed39ed39a87222cc8df3470381ef13185
Tim Visée <timvisee@gmail.com>
2016-10-26 16:25:11 +0200

    General code cleanup in Path class

--- src/main/java/io/gameoftrades/student49/Path.java
+++ src/main/java/io/gameoftrades/student49/Path.java
@@ -5,36 +5,76 @@ import io.gameoftrades.model.kaart.Stad;
 
 public class Path  {
 
+    /**
+     * Starting city.
+     */
     private Stad start;
+
+    /**
+     * Ending city.
+     */
     private Stad end;
+
+    /**
+     * Length.
+     */
     private int length;
 
+    /**
+     * Constructor.
+     *
+     * @param start Starting city.
+     * @param end Ending city.
+     * @param length Length.
+     */
     public Path(Stad start, Stad end, int length){
         this.start = start;
         this.end = end;
         this.length = length;
     }
 
+    /**
+     * Get the starting coordinate.
+     *
+     * @return Starting coordinate.
+     */
     public Coordinaat getStart(){
-        return start.getCoordinaat();
+        return this.start.getCoordinaat();
     }
 
+    /**
+     * Get the ending coordinate.
+     *
+     * @return Ending coordinate.
+     */
     public Coordinaat getEnd(){
-        return end.getCoordinaat();
+        return this.end.getCoordinaat();
     }
 
+    /**
+     * Get the starting city.
+     *
+     * @return Starting city.
+     */
     public Stad getStartCity(){
-        return start;
+        return this.start;
     }
 
+    /**
+     * Get the ending city.
+     *
+     * @return Ending city.
+     */
     public Stad getEndCity(){
-        return end;
+        return this.end;
     }
 
-
+    /**
+     * Get the length.
+     *
+     * @return Length.
+     */
     public int getLength(){
         return length;
     }
-
-
 }
