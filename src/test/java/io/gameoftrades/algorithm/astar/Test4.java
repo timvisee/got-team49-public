25319184ad126500677ea655a3e234797cc37d8c
Nathan Bakhuijzen <nathanbakhuijzen@gmail.com>
2016-10-27 16:57:20 +0200

    Create 5 test classes

---
+++ src/test/java/io/gameoftrades/algorithm/astar/Test4.java
@@ -0,0 +1,15 @@
+package io.gameoftrades.algorithm.astar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test4 {
+
+	// Word er een pad gevonden als twee steden worden gescheiden door zee?
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/io/gameoftrades/algorithm/astar/Test5.java
@@ -0,0 +1,15 @@
+package io.gameoftrades.algorithm.astar;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test5 {
+
+	// Is het resulterende pad werkelijk het snelste pad i.p.v. het kortste pad?
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
