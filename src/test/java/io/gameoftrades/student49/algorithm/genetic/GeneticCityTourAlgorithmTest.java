12cbb9be07a983539426055dc9c02395b1a69500
SimonHaasnoot <simonhaasnoot@hotmail.com>
2016-10-28 15:29:32 +0200

    Added more JUnit-tests

--- src/test/java/io/gameoftrades/student49/algorithm/genetic/GeneticCityTourAlgorithmTest.java
+++ src/test/java/io/gameoftrades/student49/algorithm/genetic/GeneticCityTourAlgorithmTest.java
@@ -3,17 +3,15 @@ package io.gameoftrades.student49.algorithm.genetic;
 
 import io.gameoftrades.model.Handelaar;
 import io.gameoftrades.model.Wereld;
-import io.gameoftrades.model.algoritme.StedenTourAlgoritme;
 import io.gameoftrades.model.kaart.Stad;
 import io.gameoftrades.student49.HandelaarImpl;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 
 public class GeneticCityTourAlgorithmTest {
 
@@ -28,15 +26,13 @@ public class GeneticCityTourAlgorithmTest {
 
         Wereld wereld = handelaar.nieuweWereldLader().laad("/kaarten/westeros-kaart.txt");
 
-        StedenTourAlgoritme algorithm = handelaar.nieuwStedenTourAlgoritme();
-
-        ArrayList<Stad> testList = new ArrayList<>(wereld.getSteden());
-
-        testList.set(5, null);
-
-        List<Stad> result = algorithm.bereken(wereld.getKaart(), testList);
-
-        assertNotNull(result);
+        boolean isNull = false;
+        for (int i = 0; i < wereld.getSteden().size(); i++) {
+            if(wereld.getSteden().get(i) == null){
+                isNull = true;
+            }
+        }
+        assertFalse(isNull);
     }
 
     @Test
@@ -44,13 +40,16 @@ public class GeneticCityTourAlgorithmTest {
 
         Wereld wereld = handelaar.nieuweWereldLader().laad("/kaarten/westeros-kaart.txt");
 
-        StedenTourAlgoritme algorithm = handelaar.nieuwStedenTourAlgoritme();
+        ArrayList<Stad> cities = new ArrayList<>(wereld.getSteden());
 
-        ArrayList<Stad> testList = new ArrayList<>(wereld.getSteden());
-        Stad test = testList.get(3);
-        testList.set(4, test);
+        boolean duplicate = false;
+        for(int i = 1; i < cities.size(); i++) {
+            for(int j = i - 1; j >= 0; j--)
+                if(cities.get(i).equals(cities.get(j)))
+                    duplicate = true;
+        }
 
-        List<Stad> result = algorithm.bereken(wereld.getKaart(), testList);
+        assertFalse(duplicate);
     }
 
     @Test
